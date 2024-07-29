e134d4f3188c1e2028dcf025dea3854caa3a128b
Stella0910 <aniretake0910@gmail.com>
2024-07-29 19:09:46 +0300

    Add files via upload

---
+++ Module1.7.py
@@ -0,0 +1,8 @@
+HW_done_count = 12
+hours_spent_count = 1.5
+name_course = 'Python'
+
+time_for_one_HW = hours_spent_count / HW_done_count
+
+print('Курс: ',name_course, ',', ' всего задач: ', HW_done_count, ',', ' затрачено часов: ',
+      hours_spent_count, ',', ' среднее время выполнения: ', time_for_one_HW, ' часа.', sep = '')
